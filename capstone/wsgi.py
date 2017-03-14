1089526e9331c9a22238978bdfd1269c951c4fc6
John Spiva <johnspiva@gmail.com>
2017-03-14 13:38:43 -0400

    added production settings file for heroku deployment

--- capstone/wsgi.py
+++ capstone/wsgi.py
@@ -11,7 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "capstone.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "capstone.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "capstone.settings_production")
 
 application = get_wsgi_application()
 
