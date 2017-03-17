7118fc29b62a729d43cd91650a7e850710e3ebf6
John Spiva <johnspiva@gmail.com>
2017-03-17 19:18:43 -0400

    removed upload model since image urls will be used now instead

--- ws_interlace/admin.py
+++ ws_interlace/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from ws_interlace.models import Worksheet, Answer, Upload
+from ws_interlace.models import Worksheet, Answer
 
 admin.site.register(Worksheet)
 admin.site.register(Answer)
-admin.site.register(Upload)
