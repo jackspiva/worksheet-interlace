8cf49e56f62ba2e4cdfc5786109d3c0415283fbb
John Spiva <johnspiva@gmail.com>
2017-04-10 11:53:46 -0400

    fixed file downloading for python3

--- ws_interlace/serializers.py
+++ ws_interlace/serializers.py
@@ -2,25 +2,9 @@ from rest_framework import serializers
 from ws_interlace.models import Worksheet, Answer
 from django.core.files import File
 import os
-import urllib
-
-
-def get_remote_image(ans):
-    if ans.image_url and not ans.image_file:
-        result = urllib.urlretrieve(ans.image_url)
-        ans.image_file.save(
-            os.path.basename(ans.image_url),
-            File(open(result[0]))
-        )
-        ans.save()
 
 
 class AnswerSerializer(serializers.HyperlinkedModelSerializer):
-
-    def create(self, validated_data):
-        answer = Answer.objects.create(**validated_data)
-        get_remote_image(answer)
-        return answer
 
     class Meta:
         model = Answer
